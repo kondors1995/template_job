d32a787ead7e7c4a157c44aec28417213ac6f0e6
owca <contact@axiomos.xyz>
2024-01-21 19:45:00 +0100

    fix problems with passing variables

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,8 +3,13 @@
 
 # put here data for build
 
-export REPO_URL="https://github.com/DerpFest-AOSP/manifest.git"
-export REPO_BRANCH="14"
-export DEVICE_NAME="raphael"
-export ROM_LAUNCH_PREFIX="derp"
-export BUILD_TYPE="userdebug"
+REPO_URL=https://github.com/DerpFest-AOSP/manifest.git
+REPO_BRANCH=14
+DEVICE_NAME=raphael
+ROM_LAUNCH_PREFIX=derp
+BUILD_TYPE=userdebug
+
+# config git becouse is needed
+git config --global user.name "customrom"
+git config --global user.email "customrom@customrom.com"
+
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,21 +1,31 @@
-#!/bin/bash
+#!/bin/bash -l
 
 source ./scripts/env.sh
 
-repo_url=$REPO_URL
-repo_branch=$REPO_BRANCH
-echo "Syncing rem sources..."
+
+echo "Syncing rom sources..."
 rom_name=$ROM_LAUNCH_PREFIX
 current_directory="$(pwd)"
-mkdir -p "${current_directory}/${rom_name}"
-rom_dir="${current_directory}/${rom_name}"
-cd $rom_dir    
+rom_dir="$current_directory/$rom_name"
+mkdir -p $rom_dir
+cd $rom_dir
 echo "Working directory:"
 pwd
+
+function repo_init() {
+    echo "Init repository..."
+    repo init -u "$REPO_URL" -b "$REPO_BRANCH"
+}
+
+function repo_sync() {
+    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+}
+
+
 if [ -d ".repo" ]; then
     echo "Repo already exists..."
     echo "Try to re sync now..."
-    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+    repo_sync
     if [ $? -eq 0 ]; then
             echo "Repo re-sync successfully."
             exit 0
@@ -24,10 +34,8 @@ if [ -d ".repo" ]; then
             exit 1
     fi
 else
-    echo "Init repo for rom... ${repo_url} - branch ${repo_branch}"
-    repo init -u $repo_url -b $repo_branch
-    echo "Sync repo ..."
-    repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+    repo_init
+    #repo_sync
     if [ $? -eq 0 ]; then
             echo "Repo sync successfully."
             exit 0
