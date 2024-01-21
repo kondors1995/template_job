32a99d45ea0b914c1492e9e3d2ec2ca9eaf27553
owca <contact@axiomos.xyz>
2024-01-21 18:07:54 +0100

    fix errors in variables

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,7 +8,7 @@ echo "Syncing rem sources..."
 rom_name=$ROM_LAUNCH_PREFIX
 current_directory="$(pwd)"
 mkdir -p "${current_directory}/${rom_name}"
-rom_dir = "${current_directory}/${rom_name}"
+rom_dir="${current_directory}/${rom_name}"
 cd rom_dir    
 if [ -d "./.repo" ]; then
     echo "Repo already exists..."
